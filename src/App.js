5227698a247debf14a546fabc276dd595466247e
Samuel Oyedeji <32018370+sammyteeoye@users.noreply.github.com>
2022-06-20 14:37:57 +0100

    Search component deleted. Search input now in CurrentWeather component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
-import { Form } from "react-bootstrap";
-import Search from './components/Search';
+import CurrentWeather from './components/CurrentWeather';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Location</h1>
-      <p>Tuesday, 29 June</p>
-      <p>14:12</p>
-      <Search />
-      
+    <div className='app--container'>
+      <CurrentWeather /> 
+      <div>
+        
+      </div>     
     </div>
   );
 }
---
+++ src/components/CurrentWeather.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { MdMyLocation, MdLocationOn } from "react-icons/md";
+
+function CurrentWeather() {
+  return (
+    <div className="current--weather">
+        <div className="search--location">
+            <form action="">
+                <input type="search" className="color--one" placeholder="Search . . ."/>
+                <button type="submit" className="color--one">
+                    <MdMyLocation className="location--icon"/>
+                </button>
+            </form>
+        </div>
+        <div className="current--weather--icon">
+            
+        </div>
+        <div className="today">
+            <p className="temp--data"><span>15</span> °C</p>
+            <p className="weather--description">Light Showers</p>
+            <p className="date">Mon, 20th June, 2022</p>
+            <div className="location">
+                <MdLocationOn className="location--icon2"/>
+                <p>Akure</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default CurrentWeather
