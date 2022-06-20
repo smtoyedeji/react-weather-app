d7c0d14d9759a8fdd0d6d42c466edcbbf35f2416
Samuel Oyedeji <32018370+sammyteeoye@users.noreply.github.com>
2022-06-20 18:12:59 +0100

    WeatherForecast component in progress

--- src/components/WeatherForecast.js
+++ src/components/WeatherForecast.js
@@ -1,16 +1,83 @@
-import React from 'react'
+import React from 'react';
+import "../WeatherForecast.css"
 
 export default function WeatherForecast(props) {
     console.log(props)
     return (
-    <div className="weather--forecast">
-        <h4>Today</h4>
-        <div>Weather Icon</div>
-        <p>Min Temp</p>
-        <p>Max Temp</p>
-        <p>Humidity</p>
-        <p>Windspeed</p>
-    </div>
+      <div className="weather--forecast">
+        <div className="weather--card">
+          <h4>Today</h4>
+          <div className="weather--icon"></div>
+          <div className="temp">
+            <p>Min</p>
+            <p>Max</p>
+          </div>
+          <div className="humidity-windspeed">
+            <p>Humi</p>
+            <p>Win</p>
+          </div>
+        </div>
+        <div className="weather--card">
+          <h4>Today</h4>
+          <div className="weather--icon"></div>
+          <div className="temp">
+            <p>Min</p>
+            <p>Max</p>
+          </div>
+          <div className="humidity-windspeed">
+            <p>Humi</p>
+            <p>Win</p>
+          </div>
+        </div>
+        <div className="weather--card">
+          <h4>Today</h4>
+          <div className="weather--icon"></div>
+          <div className="temp">
+            <p>Min</p>
+            <p>Max</p>
+          </div>
+          <div className="humidity-windspeed">
+            <p>Humi</p>
+            <p>Win</p>
+          </div>
+        </div>
+        <div className="weather--card">
+          <h4>Today</h4>
+          <div className="weather--icon"></div>
+          <div className="temp">
+            <p>Min</p>
+            <p>Max</p>
+          </div>
+          <div className="humidity-windspeed">
+            <p>Humi</p>
+            <p>Win</p>
+          </div>
+        </div>
+        <div className="weather--card">
+          <h4>Today</h4>
+          <div className="weather--icon"></div>
+          <div className="temp">
+            <p>Min</p>
+            <p>Max</p>
+          </div>
+          <div className="humidity-windspeed">
+            <p>Humi</p>
+            <p>Win</p>
+          </div>
+        </div>
+        <div className="weather--card">
+          <h4>Today</h4>
+          <div className="weather--icon"></div>
+          <div className="temp">
+            <p>Min</p>
+            <p>Max</p>
+          </div>
+          <div className="humidity-windspeed">
+            <p>Humi</p>
+            <p>Win</p>
+          </div>
+        </div>
+      </div>
   )
 }
 
